67c3d4fdc18e34a6e13c289003d2b9edc75fe686
Rafał Petryka <rafalpetryka@gmail.com>
2015-02-26 00:23:58 +0100

    Limited burgers works - add, edit and destroy

--- app/controllers/limited_burgers_controller.rb
+++ app/controllers/limited_burgers_controller.rb
@@ -11,11 +11,11 @@ class LimitedBurgersController < ApplicationController
 	end
 
 	def create
-		@limited_burger = LimitedBurger.new(limited_params)
+		@limited_burger = LimitedBurger.new(limited_burger_params)
 
 		if @limited_burger.save
 			flash[:success] = "Dodano limitowaną edycję"
-			redirect_to limited_index_path
+			redirect_to limited_burgers_path
 		end
 	end
 
@@ -24,10 +24,21 @@ class LimitedBurgersController < ApplicationController
 	end
 
 	def edit
-		@limited_burger = LimitedBurger.find(parmas[:id])
+		@limited_burger = LimitedBurger.find(params[:id])
 	end
 
-	def limited_params
-		params.require(:limited).permit(:date, :date_on_website, :name1, :name2, :composition1, :composition2)
+	def destroy
+		@limited_burger = LimitedBurger.find(params[:id])
+		if @limited_burger.present?
+			@limited_burger.destroy
+			flash[:success] = "Usunięto limitowaną edycję"
+		else
+			flash[:error] = "Nie udało się usunąć edycji limitowanej"
+		end
+		redirect_to limited_burgers_path
+	end
+
+	def limited_burger_params
+		params.require(:limited_burger).permit(:date, :date_on_website, :name1, :name2, :composition1, :composition2)
 	end
 end
