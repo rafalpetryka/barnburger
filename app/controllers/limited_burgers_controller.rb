4a865ab6f20ac2024f6a3cb4c48672b42418e86b
Rafał Petryka <rafal@ideasfactory.pl>
2015-02-26 15:46:45 +0100

    Workin on limited_burgers_controller

--- app/controllers/limited_burgers_controller.rb
+++ app/controllers/limited_burgers_controller.rb
@@ -12,7 +12,7 @@ class LimitedBurgersController < ApplicationController
 
 	def create
 		@limited_burger = LimitedBurger.new(limited_burger_params)
-
+		change_date_on_website
 		if @limited_burger.save
 			flash[:success] = "Dodano limitowaną edycję"
 			redirect_to limited_burgers_path
@@ -38,7 +38,37 @@ class LimitedBurgersController < ApplicationController
 		redirect_to limited_burgers_path
 	end
 
+
+
+	def update
+		@limited_burger = LimitedBurger.find(params[:id])
+		
+		if @limited_burger.update(limited_burger_params)
+			change_date_on_website
+
+			@limited_burger.update!(limited_burger_params)
+			redirect_to limited_burgers_path, notice: "Edytowano limitowaną edycję"
+		else
+			render 'edit'
+		end
+	end
+
 	def limited_burger_params
 		params.require(:limited_burger).permit(:date, :date_on_website, :name1, :name2, :composition1, :composition2)
+	end
+
+	#zle dziala
+	def change_date_on_website
+		if @limited_burger.date_on_website.starts_with? "od "
+			puts "-----------------"
+			puts "zaczyna sie od od"
+			puts "-----------------"
+		else
+			puts "-----------------"
+			puts "nie zaczyna sie od od"
+			puts "-----------------"
+			@limited_burger.date_on_website = "od "+@limited_burger.date_on_website
+			@limited_burger.save
+		end
 	end
 end
