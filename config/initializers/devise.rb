d62377983c4434ac4875d4157e5cbbe374b68b09
Rafał Petryka <rafalpetryka@gmail.com>
2016-06-03 17:31:23 +0200

    Change password length for development

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -140,7 +140,11 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 8..128
+  if Rails.env.development?
+    config.password_length = 1..128
+  else
+    config.password_length = 8..128
+  end
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
