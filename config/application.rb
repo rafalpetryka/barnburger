7aafda5974bcbbb1eb3a301f2b13d1b742273829
Rafał Petryka <rafal@ideasfactory.pl>
2016-06-05 15:33:03 +0200

    Add rails asset index precompile

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module Barnburger
   class Application < Rails::Application
     config.i18n.default_locale = 'pl'
-    # config.assets.precompile += %w( news/reset.css news/style.css news/index.css )
+    config.assets.precompile += %w( news/reset.css news/style.css news/index.css )
     config.time_zone = 'Warsaw'
     # config.time_zone = 'UTC'
     # config.active_record.default_timezone = :utc
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
